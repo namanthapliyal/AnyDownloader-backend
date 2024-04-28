3d4ed68132904b9c63e68ede468bd1a59dd89bbf
Devesh Kumar Sharma <deveshkumarsharma475@gmail.com>
2024-04-28 15:44:55 +0530

    enhanced the code readability for fb_views

--- src/fb_views.py
+++ src/fb_views.py
@@ -18,15 +18,15 @@ def get_downloadlink(url):
 
     url = url.replace("www", "mbasic")
     try:
-        r = get(url, timeout=5, allow_redirects=True)
-        if r.status_code != 200:
+        response = get(url, timeout = 5, allow_redirects = True)
+        if response.status_code ! =  200:
             raise HTTPError
-        a = findall("/video_redirect/", r.text)
-        if len(a) == 0:
+        allVideos = findall("/video_redirect/", response.text)
+        if len(allVideos)  =  =  0:
             print("[!] Video Not Found...")
             exit(0)
         else:
-            return unquote(r.text.split("?src=")[1].split('"')[0])
+            return unquote(response.text.split("?src = ")[1].split('"')[0])
     except (HTTPError, ConnectionError):
         print("[x] Invalid URL")
         exit(1)
@@ -37,10 +37,10 @@ def Download_vid():
 
     # Validates Link and download Video
     global Url_Val
-    url=Url_Val.get()
+    url = Url_Val.get()
 
-    Status["text"]="Downloading"
-    Status["fg"]="green"
+    Status["text"] = "Downloading"
+    Status["fg"] = "green"
 
 
     # Validating Input
@@ -49,11 +49,11 @@ def Download_vid():
         Invalid_Url()
         return
 
-    link=get_downloadlink(url)
+    link = get_downloadlink(url)
 
     start_downloading()
 
-    download_thread=VideoDownload(link)
+    download_thread = VideoDownload(link)
     download_thread.start()
     monitor(download_thread)
 
@@ -64,7 +64,7 @@ def monitor( download_thread):
     if download_thread.is_alive():
 
         try:
-            bar["value"]=queue.get(0)
+            bar["value"] = queue.get(0)
             ld_window.after(10, lambda: monitor(download_thread))
         except Empty:
             pass
@@ -83,14 +83,14 @@ class VideoDownload(Thread):
 
         # save the picture to a file
         block_size = 1024  # 1kB
-        r = get(self.url, stream=True)
-        total_size = int(r.headers.get("content-length"))
+        response = get(self.url, stream = True)
+        total_size = int(response.headers.get("content-length"))
 
         with open('video.mp4', 'wb') as file:
-            totaldata=0;
-            for data in r.iter_content(block_size):
-                totaldata+=len(data)
-                per_downloaded=totaldata*100/total_size
+            totaldata = 0;
+            for data in response.iter_content(block_size):
+                totaldata+ = len(data)
+                per_downloaded = totaldata*100/total_size
                 queue.put(per_downloaded)
                 bar['value'] = per_downloaded
                 file.write(data)
@@ -106,34 +106,34 @@ class VideoDownload(Thread):
 
 #start download
 def start_downloading():
-   bar["value"]=0;
+   bar["value"] = 0;
 
 # GUI
 
-ld_window=tk.Tk()
+ld_window = tk.Tk()
 ld_window.title("Facebook Video Downloader")
 ld_window.geometry("400x300")
 
 # Label for URL Input
-input_label= tk.Label(ld_window,text="Enter Facebook Video URL:")
+input_label =  tk.Label(ld_window,text = "Enter Facebook Video URL:")
 input_label.pack()
 
 # Input of URL
 Url_Val = tk.StringVar()
-Url_Input = tk.Entry(ld_window, textvariable=Url_Val, font=("Calibri", 9))
-Url_Input.place( x=25,y=50, width=350)
+Url_Input = tk.Entry(ld_window, textvariable = Url_Val, font = ("Calibri", 9))
+Url_Input.place( x = 25,y = 50, width = 350)
 
 # Button for Download
-Download_button = tk.Button(ld_window, text="Download", font=("Calibri", 9), command=Download_vid)
-Download_button.place(x=100, y=100, width=200)
+Download_button = tk.Button(ld_window, text = "Download", font = ("Calibri", 9), command = Download_vid)
+Download_button.place(x = 100, y = 100, width = 200)
 
 # Progress Bar
-bar = Progressbar(ld_window, length=350, style='grey.Horizontal.TProgressbar',mode='determinate')
-bar.place(y=200,width=350,x=25)
+bar = Progressbar(ld_window, length = 350, style = 'grey.Horizontal.TProgressbar',mode = 'determinate')
+bar.place(y = 200,width = 350,x = 25)
 
-queue=queue.Queue()
+queue = queue.Queue()
 # Text for Status of Downloading
-Status = tk.Label(ld_window, text="Hello!! :D", fg="blue", font=("Calibri", 9), bd=1, relief=tk.SUNKEN, anchor=tk.W, padx=3)
-Status.pack(side=tk.BOTTOM, fill=tk.X)
+Status = tk.Label(ld_window, text = "Hello!! :D", fg = "blue", font = ("Calibri", 9), bd = 1, relief = tk.SUNKEN, anchor = tk.W, padx = 3)
+Status.pack(side = tk.BOTTOM, fill = tk.X)
 
 ld_window.mainloop()
